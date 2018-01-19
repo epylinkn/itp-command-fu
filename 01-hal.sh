a779db404e944b79c47f79a774ad4a979eb525a7
Anthony Bui <anthonybui@gmail.com>
2018-01-19 11:11:24 -0500

    OMG THEY GOT RID OF BRUCE

--- 01-hal.sh
+++ 01-hal.sh
@@ -1,4 +1,6 @@
 function hal {
    open -a "Safari" http://cdn.quotesgram.com/img/39/65/1976467072-hal_9000_quotes.jpg
-   say --voice Bruce "I'm sorry Dave. I'm afraid I can't do that"
+   say --voice Fred "I'm sorry Dave. I'm afraid I can't do that"
 }
+
+alias kubrick="yes \"All work and no play makes Jack a dull boy.\""
